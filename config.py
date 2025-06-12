f5802eba7713f643d296d0546b5714beddd071dd
Sujoy ॐ <iamsujoyp@gmail.com>
2025-06-13 01:14:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ import re
 id_pattern = re.compile(r"^-?\d+$")
 
 class Config(object):
-    API_ID = os.environ.get("API_ID", "your_api_id")
-    API_HASH = os.environ.get("API_HASH", "your_api_hash")
+    API_ID = os.environ.get("API_ID", "22182189")
+    API_HASH = os.environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
-    DB_URL = os.environ.get("DB_URL", "your_mongodb_url")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://sujoy123m:wTWKGUaxYE7dxb1l@cluster0.zorxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "video_compressor")
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get("ADMIN", "").split()]
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get("ADMIN", "8181241262").split()]
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002872961182"))
     BOT_UPTIME = time.time()
     MAX_QUEUE_SIZE = 5
