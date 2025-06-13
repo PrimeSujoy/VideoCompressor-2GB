1dd37f29758231b9b350f988a9f6cd3eff813b69
Sujoy ॐ <iamsujoyp@gmail.com>
2025-06-13 10:36:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ class Bot(Client):
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
             workers=200,
-            plugins={"root": "plugins"},  # Ensure plugins directory is correctly set
+            plugins={"root": "."},  # Ensure plugins directory is correctly set
             sleep_threshold=15,
         )
 
